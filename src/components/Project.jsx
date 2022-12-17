996dca153dcb65cc2006cde709789cd89298263b
LukasMartinez <lukasmartinezc@gmail.com>
2022-12-17 18:48:32 -0300

    Se realiza sección de proyectos

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,54 @@
-const Project = ({ image, title }) => {
+import { Fragment } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEarthAmericas } from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
+const Project = ({ image, title, technologies, website, frontend, backend, repository, fullStack }) => {
   return (
-    <div className='container'>
+    <div className='flex items-center justify-center'>
         <figure className='box-image'>
             <img className="img-projects" src={image} />
             <div className="layer">
-                <h3 className="text-white font-bold">{title}</h3>
-                <p></p>
-                <div>
-
+                <h3 className="text-white font-bold text-lg mb-2">{title}</h3>
+                <p className="text-white text-sm">{technologies}</p>
+                <div className="w-full flex justify-around items-center text-white text-sm mt-3">
+                    {fullStack ? (
+                        <Fragment>
+                            <a href={website} target='_blank' className="bg-stone-900 py-1 px-2 link">
+                                <FontAwesomeIcon icon={faEarthAmericas} />
+                                <span className="pl-1">
+                                    Website
+                                </span>
+                            </a>
+                            <a href={frontend} target='_blank' className="bg-stone-900 py-1 px-2 link">
+                                <FontAwesomeIcon icon={faGithub} />
+                                <span className="pl-1 p-1">
+                                    Frontend
+                                </span>
+                            </a>
+                            <a href={backend} target='_blank' className="bg-stone-900 py-1 px-2 link">
+                                <FontAwesomeIcon icon={faGithub} />
+                                <span className="pl-1">
+                                    Backend
+                                </span>
+                            </a>
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <a href={website} target='_blank' className="bg-stone-900 py-1 px-2 link">
+                                <FontAwesomeIcon icon={faEarthAmericas} />
+                                <span className="pl-1">
+                                    Website
+                                </span>
+                            </a>
+                            <a href={repository} target='_blank' className="bg-stone-900 py-1 px-2 link">
+                                <FontAwesomeIcon icon={faGithub} />
+                                <span className="pl-1">
+                                    Repositorio
+                                </span>
+                            </a>
+                        </Fragment>
+                    )}
                 </div>
             </div>
         </figure>
